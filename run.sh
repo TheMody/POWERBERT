0a9b0907ffcc9962c9326a710770458e101a43cc
Philip <p.kenneweg@unitybox.de>
2022-09-27 11:43:49 +0200

    zeug

---
+++ run.sh
@@ -0,0 +1,9 @@
+#python main.py --config configmrpcqqp.json 
+#python main.py --config configqqpmrpc.json 
+#python main.py --config configembshftmrpc.json 
+#python main.py --config configembshftmnli.json 
+python main.py --config config.json 
+python main.py --config config_reduce.json 
+#python main.py --config configdatashftqnli.json 
+
+
